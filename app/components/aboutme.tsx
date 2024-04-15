0fe4ea52de6870c258cfe2d39a5f19175e72d779
pawel-pawel <pawelitwojcik@gmail.com>
2024-04-15 21:37:15 +0200

    images

--- app/components/aboutme.tsx
+++ app/components/aboutme.tsx
@@ -11,7 +11,10 @@ export default function Aboutme() {
             <h2 className="text-9x1 font-bold">Cześć,</h2>      
             <h1>jestem studentem Politechniki Śląskiej, a to moja strona na której chciałbym pokazać co robię w wolny czasie i nie tylko.
               Myślę że znajdzie się tu parę poradników, pochwalę się projektami które udało mi się zrobić, a także że będzie to moje większe CV
-              <h1 className="my-2">Ps. Strona ta jest na wczesnym etepie tworzenia a z każdy krokiem do przodu wymaga dużo nauki.</h1> </h1>       
+              </h1>
+              <div className="pt-3">
+              Ps. Strona ta jest na wczesnym etepie tworzenia a z każdy krokiem do przodu wymaga dużo nauki.    
+              </div>    
         </div>   
         <Divider className="my-4"/>
         <div className="flex flex-col justify-center">
--- app/homelab/page.tsx
+++ app/homelab/page.tsx
@@ -1,8 +1,44 @@
+import { Card, CardBody, CardHeader, Divider, Image } from "@nextui-org/react";
+
 export default function Home() {
-    return (
-      <>
-       Tu bedzie cos o moim homelabie
-      </>
-    );
-  }
-  
+  return (
+    <div className="flex flex-col space-x-5 justify-center lg:flex-row ">
+      <Card>
+        <CardHeader>
+          <div className="flex flex-row justify-center">
+            <h2 className="text-9x1 font-bold">HP t620</h2>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <Image
+            src="hpt620.jpg"
+            alt="HP t620 image"
+            className="object-cover rounded-xl"   
+          />
+          <div>
+            System: TruNas Scale <br></br>
+            Procesor: AMD GX-415GA Quad-Core APU with AMD Radeon HD 8330E (1.5 GHz) <br></br>
+            Ram: 16 GB DDR4 <br></br>
+            Pamięć: Oryginalne: 16GB Mój mod: 256GB M.2 (SATA), 2x2TB 3,5" HDD (RAID 1), 1,5TB 3,5" HDD
+
+          </div>
+          
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader>
+          <div className="flex flex-row justify-center">
+            <h2 className="text-9x1 font-bold">Lenovo ThinkCentre M710q</h2>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <Image
+            src="thinkcentre.jpeg"
+            alt="Lenovo Thinkcentre"
+            className="object-cover rounded-xl"
+          />
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
